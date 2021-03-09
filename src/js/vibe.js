8c498397552492218bbad21c5fdc34b343797157
Reed Murphy <reedmurphy248@gmail.com>
2021-03-09 12:08:58 -0600

    Initial Commit

---
+++ src/js/vibe.js
@@ -0,0 +1,9 @@
+const vibes = [
+    "... you are awesome!",
+    "... have a wonderful day!",
+    "... you got this!"
+];
+
+const vibe = vibes[Math.floor(Math.random() * vibes.length)];
+
+document.querySelector(".vibe").append(vibe);
